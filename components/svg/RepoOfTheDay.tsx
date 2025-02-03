1b51bff797e9351a880fe99869e66ca15d065479
TrinhNgocThanh <exviet@gmail.com>
2025-02-03 17:29:42 +0700

    OKK

--- components/svg/RepoOfTheDay.tsx
+++ components/svg/RepoOfTheDay.tsx
@@ -24,7 +24,7 @@ function RepoOfTheDay() {
         x="6"
         y="10"
       >
-        <div>FREE AI CHAT</div>
+        <div>AI API KEYs</div>
       </foreignObject>
       <svg
         height="45"
@@ -54,7 +54,7 @@ function RepoOfTheDay() {
         x="64"
         y="24"
       >
-        <div>#1 API Key Provider</div>
+        <div>#1 API duy nhất</div>
       </foreignObject>
       <foreignObject
         height="36"
